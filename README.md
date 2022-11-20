de98f7432662ccf53dfffd13d1c9c5bf28fa96d8
CPdA-Omi <charlie.perrondarc@gmail.com>
2022-11-20 16:07:59 +0100

    all: Writing of README and add bash version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nbPremiers
-A little experiment to try to make an optimised program wich calcul a certain number of prime numbers
+## A little experiment to try to make an optimised program wich calcul a certain number of prime numbers
+## The program will be write in many other programming languages later...
+
+## How to use ?
+---
+All works with arguments (one or two)
+
+- If one argument is give, the program tells you if it is prime or not
+- If two arguments are given, the program gives you all the prime numbers between both (if the first is greater than the first
+
+# Unfortnuately, all the system messages are only in french for now
---
+++ bash/nbPremiers.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+premier=true
+cpt=0
+cptLine=0
+min=$1
+max=$2
+
+if [ $# -ne 1 -a $# -ne 2 ]; then # $#>2 or $#<1
+	echo "Erreur, fournir UN ou DEUX arguments." >&2
+	exit 1
+
+elif [ $# -eq 1 ]; then #only one number
+	if [ $min -eq 1 -o $min -eq 2 -o $min -eq 3 ]; then
+		echo "$min est un nombre premier"
+	else
+		if [ $(($min%2)) -ne 0 ]; then
+			premier=true
+			sqrt=`echo "scale=0; sqrt($min)" | bc`
+			for i in $( seq 3 2 $(($sqrt+1)) ); do
+				if [ $(($min%$i)) -eq 0 ]; then
+					premier=false
+					break
+				fi
+			done
+			if [ $premier == true ]; then
+				echo "$min est un nombre premier"
+				exit 0
+			fi
+		fi
+	fi
+	echo "$min n'est pas un nombre premier"
+	exit 0
+
+elif [ $min -lt 1 -o $max -lt 1 -o $min -gt $max ]; then # $min<1 or $max<1 or $min>$max
+	echo "Erreur, nombres invalides." >&2
+	exit 1
+fi
+
+if [ $min -eq 1 -a $max -gt 2 ]; then
+	min=$(($min+1))
+fi
+
+for i in $(seq $min 1 $max); do
+	if [ $i -eq 2 -o $i -eq 3 -o $i -eq 5 ]; then
+		echo -ne "\t$i"
+		cpt=$(($cpt+1))
+		cptLine=$(($cptLine+1))
+		continue
+	else
+		if [ $(($i%2)) -ne 0 -a $(($i%5)) -ne 0 ]; then # $i is not pair or not multiple of 5
+			premier=true
+			if [ $cptLine -gt 9 ]; then
+				echo ""
+				cptLine=0
+			fi
+			sqrt=`echo "scale=0; sqrt($i)" | bc`
+			for j in $( seq 3 2 $(($sqrt+1)) ); do
+				if [ $(($i%$j)) -eq 0 ]; then
+					premier=false
+					break
+				fi
+			done
+			if [ $premier == true ]; then
+				echo -ne "\t$i"
+				cpt=$(($cpt+1))
+				cptLine=$(($cptLine+1))
+			fi
+		fi
+	fi
+done
+
+echo -e "\n\nNombres premiers : $cpt"
